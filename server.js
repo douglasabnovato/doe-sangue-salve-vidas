fae4d047e1c18c1f5b565ad38c4bf6262f83edaa
@douglasabnovato <douglasabnovato@gmail.com>
2020-10-05 15:08:49 -0300

    v6.1.7. nunjucks

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
+//configurar servidor
 const express = require('express');
 const server = express();
 
+//configurar template engine
+const nunjucks = require('nunjucks');
+nunjucks.configure()
+
+//configurar apresentação da página
 server.get('/', function(req, res){
-    return res.send("Hello, Douglas !");
+    return res.send("Hello, Douglas !!!");
 })
 
-server.listen(3000);
+server.listen(3000, function(){
+    console.log('Iniciei o servidor e exibi no console.')
+});
